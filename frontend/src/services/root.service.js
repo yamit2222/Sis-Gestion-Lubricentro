26abb4e322c51976d075ad70204edae5b675d650
Yamit <62761614+yamit2222@users.noreply.github.com>
2026-02-02 16:58:36 -0300

    Update API_URL to new base URL

--- frontend/src/services/root.service.js
+++ frontend/src/services/root.service.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import cookies from 'js-cookie';
-const API_URL = import.meta.env.VITE_BASE_URL || 'https://lubricentros.up.railway.app/api';
+const API_URL = import.meta.env.VITE_BASE_URL || 'https://elsocio.up.railway.app/api';
+
 
 
 const instance = axios.create({
